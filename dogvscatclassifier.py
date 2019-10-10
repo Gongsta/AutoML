55a51fdf2b8c153c874cff61064ae28ee6136148
Gongsta <gong.steven@hotmail.com>
2019-10-10 14:53:32 -0400

    created convertImageToArray function

--- dogvscatclassifier.py
+++ dogvscatclassifier.py
@@ -10,7 +10,7 @@ from keras.preprocessing import image
 import numpy as np
 
 training_times = [
-		60 * 60,		# 1 hour
+		60 * 60,		# 1 hour i.e. 60*60 seconds
 		60 * 60 * 2,	# 2 hours
 		60 * 60 * 4,	# 4 hours
 		60 * 60 * 8,	# 8 hours
@@ -34,6 +34,7 @@ with open('/Users/stevengong/Desktop/AutoML/train.csv', 'w') as train_csv:
     train_csv.close()
 
 test_dir = '/Users/stevengong/Desktop/AutoML/dataset/test_set' # Path to the train directory
+train_dir = '/Users/stevengong/Desktop/AutoML/dataset/training_set'
 class_dirs = [i for i in os.listdir(path=train_dir) if os.path.isdir(os.path.join(test_dir, i))]
  with open('/Users/stevengong/Desktop/AutoML/test.csv', 'w') as test_csv:
     fieldnames = ['File Name', 'Label']
@@ -54,34 +55,6 @@ x_train, y_train = load_image_dataset(csv_file_path='/Users/stevengong/Desktop/A
 
 x_test, y_test = load_image_dataset(csv_file_path='/Users/stevengong/Desktop/AutoML/dataset/test.csv', images_path='/Users/stevengong/Desktop/AutoML/dataset/test_set')
 """
-
-load_image_dataset()
-
-dir_path = '/Users/stevengong/Desktop/AutoML/dataset/training_set'
-for filename in listdir(dir_path):
-    if filename.endswith('.jpg'):
-        try:
-            img = Image.open(dir_path+"/"+filename) # open the image file
-            img.verify() # verify that it is, in fact an image
-        except (IOError, SyntaxError) as e:
-            print('Bad file:', filename)
-            #os.remove(base_dir+"\\"+filename) (Maybe)
-
-x_train = []
-dir_path = '/Users/stevengong/Desktop/AutoML/dataset/training_set'
-#IMPORTANT: YOU NEED TO CHANGE THIS TO MAKE IT WORK WITH THE READ_CSV
-for filename in train_file_names:
-    if filename.endswith('.jpg'):
-        try:
-            train_img = image.load_img(dir_path+"/"+filename) # open the image file
-            train_img = image.img_to_array(train_img)
-            train_img = np.expand_dims(train_img, axis=0)  # this line of code transforms our 3-dimensional array into a 4 dimensional one, axis is 0 since it is in the first position, index = 0
-            x_train.append(train_img)
-
-        except (IOError, SyntaxError) as e:
-            print('Bad file:', filename)
-            #os.remove(base_dir+"\\"+filename) (Maybe)
-
 
 def read_csv_file(csv_file_path):
     """Read the csv file and returns two separate list containing file names and their labels.
@@ -104,13 +77,58 @@ def read_csv_file(csv_file_path):
     return file_names, file_labels
 
 
+train_file_names, y_train = read_csv_file(train_csv)
+
+#Function to automatically transform images into NumPy arrays
+def transformImageToArray(dir_path, csv_file_location):
+    #dir_path is the path where the images are located
+    #csv_file is the a csv file containing 2 columns, 1 column for the names of the images and 1 column for the class of the image
+
+    x_values = []
+    file_names, y_values = read_csv_file(csv_file_location)
+    for file_name in file_names:
+        if file_name.endswith('.jpg'):
+            try:
+                file = image.load_img(dir_path + '/' + file_name)
+                file = image.img_to_array(file)
+                file = np.expand_dims(file, axis=0)
+                x_values.append(file)
+
+            except (IOError, SyntaxError) as e:
+                print('Bad file:', filename)
+                # os.remove(base_dir+"\\"+filename) (Maybe)
+
+    return x_values, y_values
+
+
 train_file_names, y_train = read_csv_file('/Users/stevengong/Desktop/AutoML/dataset/train.csv')
+
+train_csv = '/Users/stevengong/Desktop/AutoML/dataset/train.csv'
+train_dir = '/Users/stevengong/Desktop/AutoML/dataset/training_set' # Path to the train directory
+
+x_train, y_train = transformImageToArray(train_dir, train_csv)
+
 
 #Building the AutoML
 
+x_train = x_train.reshape(x_train.shape + (1,))
+
+x_test = x_test.reshape(x_test.shape + (1,))
 clf = ImageClassifier(verbose=True)
 clf.fit(x_train, y_train, time_limit=training_times[0])
 clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
 #y gives us the accuracy of our structure
 y=clf.evaluate(x_test, y_test)
 print(y)
+
+
+
+#Verfying if the images are corrupt
+for filename in listdir(dir_path):
+    if filename.endswith('.jpg'):
+        try:
+            img = Image.open(dir_path+"/"+filename) # open the image file
+            img.verify() # verify that it is, in fact an image
+        except (IOError, SyntaxError) as e:
+            print('Bad file:', filename)
+            #os.remove(base_dir+"\\"+filename) (Maybe)
