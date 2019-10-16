4631aa8371445e6d8d9034de0e1c198479070200
Gongsta <gong.steven@hotmail.com>
2019-10-16 17:46:14 -0400

    removed time limit to try and get more than one model trained

--- dogvscatclassifier.py
+++ dogvscatclassifier.py
@@ -144,7 +144,7 @@ x_test = x_test.reshape(x_test.shape + (1,))
 
 clf = ImageClassifier(verbose=True)
 #Chose to set the training time to 3600s for testing purposes
-clf.fit(x_train, y_train, time_limit=training_times[3])
+clf.fit(x_train, y_train)
 clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
 #y gives us the accuracy of our structure
 y=clf.evaluate(x_test, y_test)
